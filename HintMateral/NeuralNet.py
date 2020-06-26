cbd1cd0f76bb71c5563a27b43e6cf82eea96547f
Jorge Perez <jorgeperez@Jaidens-MBP-2.lan1>
2020-06-26 18:05:49 -0500

    Implemented TODO functions

--- HintMateral/NeuralNet.py
+++ HintMateral/NeuralNet.py
@@ -23,9 +23,14 @@
 
 import numpy as np
 import pandas as pd
+import math
 from sklearn.model_selection import train_test_split
 import requests
 import io
+
+# read data from public source
+url = "https://raw.githubusercontent.com/hip-hipJorge/ML-Assignment2/master/Assignment-2/ENB2012_data.csv"
+read_data = requests.get(url).content
 
 class NeuralNet:
     def __init__(self, dataFile, header=True, h=4):
@@ -34,7 +39,6 @@ class NeuralNet:
         # test refers to the testing dataset
         # h represents the number of neurons in the hidden layer
         raw_input = pd.read_csv(dataFile)
-        # TODO: Remember to implement the preprocess method
         processed_data = self.preprocess(raw_input)
         self.train_dataset, self.test_dataset = train_test_split(processed_data)
         ncols = len(self.train_dataset.columns)
@@ -62,37 +66,52 @@ class NeuralNet:
         self.deltaHidden = np.zeros((h, 1))
         self.h = h
 
-    #
-    # TODO: I have coded the sigmoid activation function, you need to do the same for tanh and ReLu
-    #
-
     def __activation(self, x, activation="sigmoid"):
         if activation == "sigmoid":
             self.__sigmoid(self, x)
-
-    #
-    # TODO: Define the derivative function for tanh, ReLu and their derivatives
-    #
+        if activation ==  "tanh":
+            self.__tanh(x)
+        if activation == "ReLu":
+            self.__ReLu(x)
 
     def __activation_derivative(self, x, activation="sigmoid"):
         if activation == "sigmoid":
             self.__sigmoid_derivative(self, x)
+        if activation ==  "tanh":
+            self.__tanh_derivative(x)
+        if activation == "ReLu":
+            self.__ReLu_derivative(x)
 
     def __sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
+
+    def __tanh(self, x):
+        return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+    def __ReLu(self, x):
+        return max(0, x)
 
     # derivative of sigmoid function, indicates confidence about existing weight
 
     def __sigmoid_derivative(self, x):
         return x * (1 - x)
 
-    #
-    # TODO: Write code for pre-processing the dataset, which would include standardization, normalization,
-    #   categorical to numerical, etc
-    #
+
+    def __tanh_derivative(self, x):
+        return ((2 * np.exp(2 * x) / (np.exp(2 * x) + 1)) * (1 - self.__tanh(x)))
+
+
+    def __ReLu_derivative(self, x):
+        if x > 0:
+            return 1
+        elif x < 0:
+            return 0
+        else:
+            return math.nan
 
     def preprocess(self, X):
-
+        # Add all of the dataset except the last 2 rows
+        X = X[X.columns[0:8]]
         return X
 
     # Below is the training function
@@ -126,11 +145,22 @@ class NeuralNet:
         # pass our inputs through our neural network
         in_hidden = np.dot(self.X, self.W_hidden) + self.Wb_hidden
         # TODO: I have coded the sigmoid activation, you have to do the rest
+        ## Hidden Node Pass
         if activation == "sigmoid":
             self.X_hidden = self.__sigmoid(in_hidden)
+        if activation == 'tanh':
+            self.X_hidden = self.__tanh(in_hidden)
+        if activation == 'ReLu':
+            self.X_hidden = self.__ReLu(in_hidden)
         in_output = np.dot(self.X_hidden, self.W_output) + self.Wb_output
+
+        ## Output Node Pass
         if activation == "sigmoid":
             out = self.__sigmoid(in_output)
+        if activation == 'tanh':
+            out = self.__tanh(in_output)
+        if activation == 'ReLu':
+            out = self.__ReLu(in_output)
         return out
 
     def backward_pass(self, out, activation):
@@ -138,18 +168,22 @@ class NeuralNet:
         self.compute_output_delta(out, activation)
         self.compute_hidden_delta(activation)
 
-    # TODO: Implement other activation functions
-
     def compute_output_delta(self, out, activation="sigmoid"):
         if activation == "sigmoid":
             delta_output = (self.y - out) * (self.__sigmoid_derivative(out))
-
+        if activation == "tanh":
+            delta_output = (self.y - out) * (self.__tanh_derivative(out))
+        if activation == "ReLu":
+            delta_output = (self.y - out) * (self.__ReLu_derivative(out))
         self.deltaOut = delta_output
 
     def compute_hidden_delta(self, activation="sigmoid"):
         if activation == "sigmoid":
             delta_hidden_layer = (self.deltaOut.dot(self.W_output.T)) * (self.__sigmoid_derivative(self.X_hidden))
-
+        if activation == "tanh":
+            delta_hidden_layer = (self.deltaOut.dot(self.Wb_output.T)) * (self.__tanh_derivative(self.X_hidden))
+        if activation == "ReLu":
+            delta_hidden_layer = (self.deltaOut.dot(self.Wb_output.T)) * (self.__ReLu_derivative(self.X_hidden))
         self.deltaHidden = delta_hidden_layer
 
     # TODO: Implement the predict function for applying the trained model on the  test dataset.
@@ -158,21 +192,16 @@ class NeuralNet:
 
     def predict(self, header = True):
         # TODO: obtain prediction on self.test_dataset
+
         return 0
 
-
-url = "https://docs.google.com/spreadsheets/d/1ZqlnI482tRHO7kI-FH5uxyM2bFBo_J3B/edit#gid=987132181"
-
-# read data from public source
-read_data = requests.get(url).content
-df = pd.read_csv(io.StringIO(read_data.decode('utf-8')), delimiter='\t')
+df = pd.read_csv(io.StringIO(read_data.decode('utf-8')))
 
 if __name__ == "__main__":
-    '''
+
     neural_network = NeuralNet("train.csv")
     neural_network.train()
     testError = neural_network.predict()
     print("Test error = " + str(testError))
-    print()
-    '''
-    print(df.head())
+
+
